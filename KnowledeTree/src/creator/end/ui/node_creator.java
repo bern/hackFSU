b89fc5187dad9493428078d01228305ef1130e39
Bernard Marger <bern422@gmail.com>
2014-04-05 16:36:40 -0400

    ui with collin basics

---
+++ KnowledeTree/src/creator/end/ui/node_creator.java
@@ -0,0 +1,34 @@
+package creator.end.ui;
+
+import java.awt.*;
+import javax.swing.*;
+import java.util.ArrayList;
+
+public class node_creator extends JPanel {
+	
+	public static JLabel node_name_label;
+	public static JLabel node_name;
+	public static JLabel node_body_label;
+	public static JTextArea node_body;
+	public static JLabel node_pred_label;
+	public static ArrayList<JLabel> node_pred;
+	
+	public node_creator() {
+		setLayout(new GridLayout(6,1));
+		
+		node_name_label = new JLabel("Name: ");
+		node_name = new JLabel("[No node selected]");
+		
+		node_body_label = new JLabel("Body: ");
+		node_body = new JTextArea();
+		node_body.setText("N/A");
+		
+		node_pred_label = new JLabel("Predecessors: ");
+		
+		add(node_name_label);
+		add(node_name);
+		add(node_body_label);
+		add(node_body);
+		add(node_pred_label);
+	}
+}
