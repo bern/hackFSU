eb134b6e56868e6698c131c61ec755ae85799812
Bernard Marger <bern422@gmail.com>
2014-04-05 16:54:37 -0400

    Working on layout

--- KnowledeTree/src/creator/end/ui/main_frame.java
+++ KnowledeTree/src/creator/end/ui/main_frame.java
@@ -1,12 +1,15 @@
 package creator.end.ui;
 import java.awt.*;
+
 import javax.swing.*;
-import javax.swing.border.MatteBorder;
+import javax.swing.Box;
+import javax.swing.border.Border;
 
 public class main_frame extends JFrame {
 	
 	public static JPanel main_panel;
-	public static node_creator node_panel;
+	public static JPanel node_panel;
+	public static node_creator node_;
 	
 	public static void main(String[] args) {
 			main_frame Creator_UI = new main_frame("Test UI");
@@ -22,10 +25,22 @@ public class main_frame extends JFrame {
 		main_panel = new JPanel();
 		main_panel.setLayout(new BoxLayout(main_panel, BoxLayout.X_AXIS));
 		
-		JLabel test1 = new JLabel ("test1");
+		Component x = Box.createRigidArea(new Dimension(150, 500));
+		
+		x.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+		
+		main_panel.add(x);
+		
+		//JLabel test1 = new JLabel ("test1");
 		node_panel = new node_creator();
 		
-		main_panel.add(test1);//, BorderLayout.WEST);
+		Border a = BorderFactory.createEmptyBorder(10, 30, 10, 30);
+		Border b = BorderFactory.createLineBorder(Color.BLACK, 2);
+		Border combined = BorderFactory.createCompoundBorder(b, a);
+		
+		node_panel.setBorder(combined);
+		
+		//main_panel.add(test1);//, BorderLayout.WEST);
 		main_panel.add(node_panel);//, BorderLayout.CENTER);
 		
 		add(main_panel);
